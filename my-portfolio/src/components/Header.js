ee2c5c539d450406a598a038e708cd1d03aadc6a
Aashish Dewangan <aashishdew27@gmail.com>
2024-06-03 17:42:30 +0530

    new project

---
+++ my-portfolio/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header id="header">
+      <h1>Aashish Dewangan</h1>
+      <h2>Welcome to My Portfolio</h2>
+      <p>Discover my projects and skills</p>
+      <a href="#about" className="down-arrow">▼</a>
+    </header>
+  );
+};
+
+export default Header;
